e33430b37d612b11b8ee64694c0e82661ec637d6
Wei Hung <wayne930242@gmail.com>
2024-02-11 22:04:21 +0800

    Add back btn

--- app/walking-dead/page.tsx
+++ app/walking-dead/page.tsx
@@ -1,5 +1,8 @@
 import { TableSelector } from "@/components/tableSelector";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 
 const Page = () => {
   return (
@@ -17,13 +20,18 @@ const Page = () => {
           <div className="absolute bottom-0 left-0 w-full h-full bg-black bg-opacity-50 p-4 flex justify-start items-end">
             <TableSelector />
           </div>
+          <div className="absolute top-0 left-0 p-4">
+            <Link href="/">
+              <Button variant="secondary" className="w-10 h-10 p-2 rounded-full">
+                <ArrowLeftIcon className="w-6 h-6" />
+              </Button>
+            </Link>
+          </div>
           <h1 className="absolute text-4xl font-bold text-white p-4 right-0 bottom-0 drop-shadow-xl">
             The Walking Dead
           </h1>
         </div>
-        <div>
-          
-        </div>
+        <div></div>
       </div>
     </main>
   );
